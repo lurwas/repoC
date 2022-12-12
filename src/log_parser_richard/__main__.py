5e8d4f9e122d0cad7df99ef24d5b22c55e7a8970
richard <lurwas@emacs.se>
2022-12-12 14:25:30 +0100

    remove pylint comment
    * remove pylint comment in __main__.py since it was a relic
      from where the code resided in __init__.py.

--- src/log_parser_richard/__main__.py
+++ src/log_parser_richard/__main__.py
@@ -8,9 +8,6 @@ from log_parser_richard.log_parser import LogParser
 def main():
     """Main function"""
     parser = LogParser()
-    # Since pylint sees every variable in __init__
-    # as a constant, we need to disable the warning
-    # pylint: disable=invalid-name
     result = parser.run(sys.argv)
     if not result:
         sys.exit(2)
