12739b8651a40348473e1eee28f441047d77426f
richard <lurwas@emacs.se>
2022-12-12 11:22:45 +0100

    use invalid-name instead of C0103 in pylint
    * disable invalid-name using invalid-name instead of C0103

--- src/log_parser_richard/__init__.py
+++ src/log_parser_richard/__init__.py
@@ -6,6 +6,9 @@ from log_parser_richard.log_parser import LogParser
 
 if __name__ == '__main__':
     parser = LogParser()
+    # Since pylint sees every variable in __init__
+    # as a constant, we need to disable the warning
+    # pylint: disable=invalid-name
     result = parser.run(sys.argv)
     if not result:
         sys.exit(2)
